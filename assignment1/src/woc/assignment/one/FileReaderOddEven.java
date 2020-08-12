342faed01c6e0dd7da28a17e68c6435e0a12b7d3
Rwik Mukhopadhyay <rwikee@gmail.com>
2020-08-12 17:05:59 +0530

    hardcoded path

--- assignment1/src/woc/assignment/one/FileReaderOddEven.java
+++ assignment1/src/woc/assignment/one/FileReaderOddEven.java
@@ -4,28 +4,34 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
 
 public class FileReaderOddEven {
     public static void main(String[] args) {
         try{
-            File fl = new File("sample.txt");
+
+            //String filePath = Paths.get("").toAbsolutePath().toString()+"/woc/assignment/one/"+"sample.txt";
+            File fl = new File("/home/mi/IdeaProjects/assignment1/src/woc/assignment/one/sample.txt");
             FileReader fr = new FileReader(fl);
             BufferedReader br=new BufferedReader(fr);
             String line="";
             String separator = "->";
-            while (null!=br.readLine()){
+            while ((line=br.readLine())!=null){
+
                 String[] strARR = line.split(" ");
                 for(String key : strARR){
-                    System.out.println(key+separator);
-                    if(separator.equals("->"))
-                        separator = "<-";
-                    else
-                        separator = "->";
+                    System.out.print(key+separator);
                 }
+                System.out.println("");
+                if(separator.equals("->"))
+                    separator = "<-";
+                else
+                    separator = "->";
             }
 
         }catch (Exception e){
             System.out.println("Caught exception"+e.getStackTrace());
+
         }
     }
 }
